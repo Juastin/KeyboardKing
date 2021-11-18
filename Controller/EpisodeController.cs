f50763e41b0a856c2a91b6374baa6ed7d6dbc58d
Justin Passchier <juspasschier@gmail.com>
2021-11-18 12:28:40 +0100

    Added episode controller

---
+++ Controller/EpisodeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Controller
+{
+    public static class EpisodeController
+    {
+        private static Episode _currentEpisode;
+        private static EpisodeStep _currentEpisodeStep;
+        private static int _wordIndex;
+
+        public static string Word { get => _currentEpisodeStep.Word; }
+    }
+}
