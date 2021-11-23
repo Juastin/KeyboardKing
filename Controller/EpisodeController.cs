8daa5dfe216f8190fa0d923b5c146b17ee487b1f
Wouter <woutervandervelde2000@hotmail.com>
2021-11-23 13:28:04 +0100

    added summaries to EpisodeController

--- Controller/EpisodeController.cs
+++ Controller/EpisodeController.cs
@@ -9,6 +9,9 @@ using Model;
 
 namespace Controller
 {
+    /// <summary>
+    /// Class <c>EpisodeController</c> handles and checks the user input compared to the current episode.
+    /// </summary>
     public static class EpisodeController
     {
         private static Episode _currentEpisode;
@@ -23,6 +26,13 @@ namespace Controller
         public static string WordOverlayCorrect { get =>_currentEpisodeStep?.Word.Substring(0, _wordIndex); }
         public static string WordOverlayWrong { get =>_currentEpisodeStep?.Word.Substring(0, _wrongIndex); }
 
+        /// <summary>
+        /// <para>
+        ///     <c>Initilise</c> needs to be called before starting an episode.  
+        /// </para>
+        /// Initialises all variables needed to play and keep track of the episode.
+        /// </summary>
+        /// <param name="episode">The episode that is going to be played</param>
         public static void Initialise(Episode episode)
         {
             _currentEpisode = episode;
@@ -31,6 +41,10 @@ namespace Controller
             _wrongIndex = 0;
             NextEpisodeStep();
         }
+
+        /// <summary>
+        /// Tries to dequeue next episode step from the current episode.
+        /// </summary>
         private static void NextEpisodeStep()
         {
             _wordIndex = 0;
@@ -43,6 +57,11 @@ namespace Controller
             WordChanged?.Invoke(null, new EventArgs());
         }
 
+        /// <summary>
+        /// <para>Proccesses the given bool.</para>
+        /// Will call <see cref="NextEpisodeStep"/> once the word has been correctly and fully typed.
+        /// </summary>
+        /// <param name="isCorrect"></param>
         private static void NextLetter(bool isCorrect)
         {
             if (isCorrect)
@@ -59,6 +78,10 @@ namespace Controller
                 WordChanged?.Invoke(null, new EventArgs());
         }
 
+        /// <summary>
+        /// Checks user input and parses the result to <see cref="NextLetter(bool)"/>
+        /// </summary>
+        /// <param name="input">Input from the user</param>
         public static void CheckInput(char input)
         {
             NextLetter(_currentEpisodeStep.Word[_wordIndex].Equals(input));
