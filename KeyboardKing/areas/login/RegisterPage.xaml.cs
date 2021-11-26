b135482071174690b7040c35c2e9a6f6c383fc43
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2021-11-26 12:07:47 +0100

    Added password validation check

--- KeyboardKing/areas/login/RegisterPage.xaml.cs
+++ KeyboardKing/areas/login/RegisterPage.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Controller;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace KeyboardKing.areas.login
 {
@@ -54,21 +55,35 @@ namespace KeyboardKing.areas.login
                 {
                     if (password.Equals(passwordcheck, StringComparison.Ordinal)) //Checking if the user has entered the password correctly
                     {
-                        byte[] salt = TripleDES.CreateSalt();
-                        byte[] passHashed = TripleDES.HashPassword(password, salt); //Hashing the password
-                        bool Adduser = DBQueries.AddUser(username, email, Convert.ToBase64String(passHashed), Convert.ToBase64String(salt)); //Adding new user to database
-                        if (Adduser)
+                        if (IsPasswordValid(password))
                         {
-                            ClearText();
-                            Navigate("LoginPage"); //Returning to loginpage
+                            byte[] salt = TripleDES.CreateSalt();
+                            byte[] passHashed = TripleDES.HashPassword(password, salt); //Hashing the password
+                            bool Adduser = DBQueries.AddUser(username, email, Convert.ToBase64String(passHashed), Convert.ToBase64String(salt)); //Adding new user to database
+                            if (Adduser)
+                            {
+                                ClearText();
+                                Navigate("LoginPage"); //Returning to loginpage
+                            }
+                            else { error.Content = "Error: Service onberijkbaar / Bestaande gebruiker"; }
                         }
-                        else { error.Content = "Error: Service onberijkbaar / Bestaande gebruiker"; }
+                        else { error.Content = "Error: Wachtwoord bevat geen kleine of grote letter, nummer of minstens 8 tekens"; }
                     }
                     else { error.Content = "Error: Wachtwoorden komen niet overeen"; }
                 }
                 else { error.Content = "Error: Geen geldige E-mail"; }
             }
             else { error.Content = "Error: Lege velden"; }
+        }
+
+        private static bool IsPasswordValid(string password)
+        {
+            Regex containNumber = new(@"[0-9]+");
+            Regex containUpperCase = new(@"[A-Z]+");
+            Regex containLowerCase = new(@"[a-z]+");
+            Regex containMinLength8Char = new(@".{8,}");
+
+            return containNumber.IsMatch(password) && containUpperCase.IsMatch(password) && containLowerCase.IsMatch(password) && containMinLength8Char.IsMatch(password);
         }
 
         private void BToLogin(object sender, RoutedEventArgs e)
