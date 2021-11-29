f4b27df514cd2ec7be7fd64c80b5f12184912991
Justin Passchier <juspasschier@gmail.com>
2021-11-29 13:56:46 +0100

    Added new unittest

--- ControllerTest/Controller_EpisodeController_FinishEpisodeShould.cs
+++ ControllerTest/Controller_EpisodeController_FinishEpisodeShould.cs
@@ -5,24 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Controller;
+using Model;
 
 namespace ControllerTest
 {
     public class Controller_EpisodeController_FinishEpisodeShould
     {
-        public DateTime startTime;
+        public DateTime StartTime { get; set; }
+        public Episode Episode { get; set; }
         [SetUp]
         public void SetUp()
         {
-            startTime = DateTime.Now;
+            StartTime = DateTime.Now;
+            Episode = new Episode();
+            EpisodeStep e1 = new EpisodeStep();
+            EpisodeStep e2 = new EpisodeStep();
+            e1.Word = "kaas";
+            e2.Word = "koekje";
+            Episode.EpisodeSteps.Enqueue(e1);
+            Episode.EpisodeSteps.Enqueue(e2);
         }
 
         [Test]
         public void CalculateTime_ReturnsCorrect()
         {
-           TimeSpan diffTime = EpisodeController.CalculateTime(startTime);
-           TimeSpan tsStartTime = TimeSpan.FromTicks(startTime.Ticks);
+           TimeSpan diffTime = EpisodeController.CalculateTime(StartTime);
+           TimeSpan tsStartTime = TimeSpan.FromTicks(StartTime.Ticks);
            Assert.AreNotEqual(tsStartTime, diffTime);
+        }
+
+        [Test]
+        public void CalculateScore_ReturnCorrect()
+        {
+            int maxScore = EpisodeController.CalculateMaxScore(Episode);
+            Assert.AreEqual(10, maxScore);
+            //EpisodeController.CalculateScore();
         }
     }
 }
