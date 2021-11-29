6672c33e2393e35972604cef5f37a9add2869409
Justin Passchier <juspasschier@gmail.com>
2021-11-29 13:49:59 +0100

    Added file for finishEpisodeShould and aadded unittest

---
+++ ControllerTest/Controller_EpisodeController_FinishEpisodeShould.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Controller;
+
+namespace ControllerTest
+{
+    public class Controller_EpisodeController_FinishEpisodeShould
+    {
+        public DateTime startTime;
+        [SetUp]
+        public void SetUp()
+        {
+            startTime = DateTime.Now;
+        }
+
+        [Test]
+        public void CalculateTime_ReturnsCorrect()
+        {
+           TimeSpan diffTime = EpisodeController.CalculateTime(startTime);
+           TimeSpan tsStartTime = TimeSpan.FromTicks(startTime.Ticks);
+           Assert.AreNotEqual(tsStartTime, diffTime);
+        }
+    }
+}
