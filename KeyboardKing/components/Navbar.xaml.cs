d7d8fbf9e1aa6a8030380801e536d513e2151b22
Wouter <woutervandervelde2000@hotmail.com>
2021-12-01 23:48:01 +0100

    started dynamic styling, getting value from binding happens before CurrentPage value is set

--- KeyboardKing/components/Navbar.xaml.cs
+++ KeyboardKing/components/Navbar.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,20 +25,48 @@ namespace KeyboardKing.components
     {
         public static readonly DependencyProperty CurrentPageProperty =
             DependencyProperty.Register(
-                "CurrentPage", typeof(Pages),
-                typeof(Navbar),
-                new PropertyMetadata(Pages.ChaptersPage)
+                "CurrentPage", typeof(Model.Page),
+                typeof(Navbar)
             );
 
-        public Pages CurrentPage
+        public Model.Page CurrentPage
         {
-            get { return (Pages)GetValue(CurrentPageProperty); }
+            get { return (Model.Page)GetValue(CurrentPageProperty); }
             set { SetValue(CurrentPageProperty, value); }
         }
+
+        public Brush ChaptersPageBackground { get => CurrentPage == Model.Page.ChaptersPage ? Brushes.White : Brushes.Transparent; }
+        public Brush FavoritesPageBackground { get => CurrentPage == Model.Page.FavoritesPage ? Brushes.White : Brushes.Transparent; }
+        public Brush MatchOverviewPageBackground { get => CurrentPage == Model.Page.MatchOverviewPage ? Brushes.White : Brushes.Transparent; }
+        public Brush SettingsPageBackground { get => CurrentPage == Model.Page.SettingsPage ? Brushes.White : Brushes.Transparent; }
+
+        public Brush ChaptersPageForground { get => CurrentPage == Model.Page.ChaptersPage ? Brushes.Black : Brushes.White; }
+        public Brush FavoritesPageForground { get => CurrentPage == Model.Page.FavoritesPage ? Brushes.Black : Brushes.White; }
+        public Brush MatchOverviewPageForground { get => CurrentPage == Model.Page.MatchOverviewPage ? Brushes.Black : Brushes.White; }
+        public Brush SettingsPageForground { get => CurrentPage == Model.Page.SettingsPage ? Brushes.Black : Brushes.White; }
 
         public Navbar()
         {
             InitializeComponent();
+            //switch (CurrentPage)
+            //{
+            //    case Pages.ChaptersPage:
+            //        Button_ChaptersPage.Background = Brushes.Black;
+            //        Button_ChaptersPage.Foreground = Brushes.White;
+            //        break;
+            //    case Pages.FavoritesPage:
+            //        Button_ChaptersPage.Background = Brushes.Black;
+            //        Button_ChaptersPage.Foreground = Brushes.White;
+            //        break;
+            //    case Pages.MatchOverviewPage:
+            //        Button_ChaptersPage.Background = Brushes.Black;
+            //        Button_ChaptersPage.Foreground = Brushes.White;
+            //        break;
+            //    case Pages.SettingsPage:
+            //        Button_ChaptersPage.Background = Brushes.Black;
+            //        Button_ChaptersPage.Foreground = Brushes.White;
+            //        break;
+            //}
         }
     }
 }
--- Model/Page.cs
+++ Model/Page.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public enum Pages
+    public enum Page
     {
         LoginPage,
         RegisterPage,
