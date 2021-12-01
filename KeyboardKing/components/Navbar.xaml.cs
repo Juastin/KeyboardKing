29d20be720f05eed8b0665836e4718440cce8a94
Wouter <woutervandervelde2000@hotmail.com>
2021-12-01 21:07:46 +0100

    added navbar as component

---
+++ KeyboardKing/components/Navbar.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KeyboardKing.core;
+using Model;
+
+namespace KeyboardKing.components
+{
+    /// <summary>
+    /// Interaction logic for Navbar.xaml
+    /// </summary>
+    public partial class Navbar : UserControl
+    {
+        public static readonly DependencyProperty CurrentPageProperty =
+            DependencyProperty.Register(
+                "CurrentPage", typeof(Pages),
+                typeof(Navbar),
+                new PropertyMetadata(Pages.ChaptersPage)
+            );
+
+        public Pages CurrentPage
+        {
+            get { return (Pages)GetValue(CurrentPageProperty); }
+            set { SetValue(CurrentPageProperty, value); }
+        }
+
+        public Navbar()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Model/Pages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public enum Pages
+    {
+        LoginPage,
+        RegisterPage,
+        RegisterSkillPage,
+        ChaptersPage,
+        FavoritesPage,
+        SettingsPage,
+        EpisodePage,
+        EpisodeResultPage,
+        MatchLobbyPage,
+        MatchOverviewPage,
+        MatchPlayingPage,
+        MatchResultPage
+    }
+}
