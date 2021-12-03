43cb514c9913bb312452690f6697691d2405f99d
Justin Passchier <juspasschier@gmail.com>
2021-12-03 16:52:24 +0100

    Added summaries to Controleler

--- Controller/MatchController.cs
+++ Controller/MatchController.cs
@@ -8,11 +8,21 @@ namespace Controller
 {
     public static class MatchController
     {
+        /// <summary>
+        /// <para>This method checks if there is already a user in a match.</para>
+        /// It does this by running an sql query to get all user that are in a match and compare that to the user that is in the session (the logged in user)
+        /// </summary>
+        /// <returns></returns>
         public static bool CheckIfUserExists()
         {
             string[] user = (string[])Session.Get("student");
             return DBQueries.GetAllUsersInMatch().Select(match => match.Contains(user[0])).FirstOrDefault();
         }
+        /// <summary>
+        /// <para>This method will make a match by adding a Match, and a MatchProgress to the database</para>
+        /// It does this by getting the id of the just inserted Match and inserting it into MatchProgress with the current user.
+        /// </summary>
+        /// <param name="selectedValue"></param>
         public static void MakeMatch(int selectedValue)
         {
             DBQueries.AddMatchProgress(DBQueries.AddMatch(selectedValue, (string[])Session.Get("student")), (string[])Session.Get("student"));
