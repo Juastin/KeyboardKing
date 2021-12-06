3a7b4efd1d9d7cb14fb3d7a5c408978603c442e4
storm <stormtersteeglive@hotmail.com>
2021-12-06 14:42:34 +0100

    Added comments and Exception

--- Model/UList.cs
+++ Model/UList.cs
@@ -9,6 +9,9 @@ namespace Model
 
     public class UList
     {
+        /// <summary>
+        /// Property for the content of the list.
+        /// </summary>
         private object[] _data {get;set;}
 
         public UList(object[] data)
@@ -16,9 +19,19 @@ namespace Model
             _data = data;
         }
 
+        /// <summary>
+        /// Method to get and cast an object from the _data array.
+        /// T is the expected type.
+        /// </summary>
         public T Get<T>(int index)
         {
-            return (T)Convert.ChangeType(_data[index], typeof(T));
+            try
+            {
+                return (T)Convert.ChangeType(_data[index], typeof(T));
+            } catch
+            {
+                throw new Exception("Given generic type does not match the object origin type.");
+            }
         }
     }
 }
