10658bd34543e0ed244aae5c87eea211879afbac
CamielYang <Yangcamiel@gmail.com>
2021-12-06 12:54:24 +0100

    fix errors in conflict fix

--- KeyboardKing/MainWindow.xaml.cs
+++ KeyboardKing/MainWindow.xaml.cs
@@ -19,7 +19,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Controller;
 using Model;
 using Model.event_args;
 using KeyboardKing.areas.info;
