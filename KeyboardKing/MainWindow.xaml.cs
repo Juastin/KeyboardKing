5e4c35b113e138d005e50a75d2d49044500e0ae3
CamielYang <Yangcamiel@gmail.com>
2021-12-07 11:24:09 +0100

    removed background uri in theme class and moved images in themedirectory

--- KeyboardKing/MainWindow.xaml.cs
+++ KeyboardKing/MainWindow.xaml.cs
@@ -89,9 +89,9 @@ namespace KeyboardKing
 
             _themes = new()
             {
-                {"Light", new Theme("Light Theme", "resources/themes/LightTheme.xaml", "resources/images/kk_background_4K.png")},
-                {"Dark", new Theme("Dark Theme", "resources/themes/DarkTheme.xaml", "resources/images/kk_background_dark.png")},
-                {"Space", new Theme("Space Theme", "resources/themes/SpaceTheme.xaml", "resources/images/space_theme_background.png")},
+                {"Light", new Theme("Light Theme", "resources/themes/LightTheme.xaml")},
+                {"Dark", new Theme("Dark Theme", "resources/themes/DarkTheme.xaml")},
+                {"Space", new Theme("Space Theme", "resources/themes/SpaceTheme.xaml")}
             };
 
             CBTheme.ItemsSource = _themes;
@@ -193,7 +193,6 @@ namespace KeyboardKing
         {
             themeDictionary.Clear();
             themeDictionary.MergedDictionaries.Add(new ResourceDictionary() { Source = theme.ThemeUri });
-            MainBackground.ImageSource = new BitmapImage(theme.BackgroundUri);
         }
 
         private void CBTheme_SelectionChanged(object sender, SelectionChangedEventArgs e)
