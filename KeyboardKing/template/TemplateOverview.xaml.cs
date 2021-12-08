6d2f6b40900499c12974a0c8fd772d40dd0881ad
CamielYang <Yangcamiel@gmail.com>
2021-12-08 21:32:48 +0100

    Added ListView and header template (listviewitem not yet)

--- KeyboardKing/template/TemplateOverview.xaml.cs
+++ KeyboardKing/template/TemplateOverview.xaml.cs
@@ -22,6 +22,22 @@ namespace KeyboardKing.template
         public TemplateOverview()
         {
             InitializeComponent();
+            List<User> items = new List<User>();
+            items.Add(new User() { Name = "John Doe", Age = 42, Mail = "john@doe-family.com" });
+            items.Add(new User() { Name = "Jane Doe", Age = 39, Mail = "jane@doe-family.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 7, Mail = "sammy.doe@gmail.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 7, Mail = "sammy.doe@gmail.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 7, Mail = "sammy.doe@gmail.com" });
+            lvUsers.ItemsSource = items;
         }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Mail { get; set; }
     }
 }
