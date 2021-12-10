9cd41df195426e917ad92dd0eec7d26818385f00
storm <stormtersteeglive@hotmail.com>
2021-12-10 14:15:44 +0100

    Added username to the query

--- Controller/DBQueries.cs
+++ Controller/DBQueries.cs
@@ -234,7 +234,7 @@ namespace Controller
 
         public static List<List<string>> GetOpponentProgress(int user_id, int match_id)
         {
-            SqlCommand cmd = new SqlCommand("SELECT TOP 4 progress FROM [dbo].[MatchProgress] WHERE userid != @userid AND matchid = @matchid ORDER BY progress DESC");
+            SqlCommand cmd = new SqlCommand("SELECT TOP 4 username, progress FROM [dbo].[MatchProgress] mp LEFT JOIN [dbo].[User] u ON mp.userid=u.id WHERE userid != @userid AND matchid = @matchid ORDER BY progress DESC");
 
             SqlParameter q_user_id = new SqlParameter("@userid", SqlDbType.Int, 255);
             SqlParameter q_match_id = new SqlParameter("@matchid", SqlDbType.Int, 255);
