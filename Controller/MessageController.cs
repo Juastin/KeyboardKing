577e69b7c5665099c7d96134d3f84f1a6edfc8fe
Justin Passchier <juspasschier@gmail.com>
2021-12-12 20:59:08 +0100

    changed temp variables to have correct namegiving

--- Controller/MessageController.cs
+++ Controller/MessageController.cs
@@ -4,15 +4,15 @@ namespace Controller
 {
     public static class MessageController
     {
-        public static void Show(Pages info_page, string message, Pages redirect_page, int? time)
+        public static void Show(Pages infoPage, string message, Pages redirectPage, int? time)
         {
-            Session.Add("MessagePageInfo", new UList(new object[]{message, redirect_page, time}));
-            NavigationController.NavigateToPage(info_page);
+            Session.Add("MessagePageInfo", new UList(new object[]{message, redirectPage, time}));
+            NavigationController.NavigateToPage(infoPage);
         }
-        public static void ShowConfirmation(Pages confirm_page, string message, Pages previous_page, Pages approved_page)
+        public static void ShowConfirmation(Pages confirmPage, string message, Pages previousPage, Pages approvedPage)
         {
-            Session.Add("ConfirmationPageInfo", new UList(new object[] { message, previous_page, approved_page }));
-            NavigationController.NavigateToPage(confirm_page);
+            Session.Add("ConfirmationPageInfo", new UList(new object[] { message, previousPage, approvedPage }));
+            NavigationController.NavigateToPage(confirmPage);
         }
     }
 }
