16311661b3d9e3953646c10d9701beec69badb95
Justin Passchier <juspasschier@gmail.com>
2021-12-16 14:53:00 +0100

    Moved theme selector from mainwindow to settingspage

--- KeyboardKing/areas/main/SettingsPage.xaml.cs
+++ KeyboardKing/areas/main/SettingsPage.xaml.cs
@@ -13,17 +13,59 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Controller;
+using Model;
 
 namespace KeyboardKing.areas.main
 {
+
     /// <summary>
     /// Interaction logic for SettingsPage.xaml
     /// </summary>
     public partial class SettingsPage : JumpPage
     {
-        public SettingsPage(MainWindow w) : base(w)
+        private Dictionary<string, Theme> _themes;
+        private ResourceDictionary themeDictionary = Application.Current.Resources.MergedDictionaries[0];
+
+        
+
+public SettingsPage(MainWindow w) : base(w)
         {
             InitializeComponent();
+            
+            _themes = new()
+            {
+                { "Light", new Theme("Light Theme", "resources/themes/LightTheme.xaml") },
+                { "Dark", new Theme("Dark Theme", "resources/themes/DarkTheme.xaml") },
+                { "Space", new Theme("Space Theme", "resources/themes/SpaceTheme.xaml") },
+                { "Chinese", new Theme("Chinese Theme", "resources/themes/ChineseTheme.xaml") },
+                { "Paint", new Theme("Paint Theme", "resources/themes/PaintTheme.xaml") },
+                { "Obsidian", new Theme("Obsidian Theme", "resources/themes/ObsidianTheme.xaml") },
+                { "Hello beertje", new Theme("Hello beertje", "resources/themes/HelloBeertjeTheme.xaml") },
+                { "Christmas", new Theme("Christmas Theme", "resources/themes/ChristmasTheme.xaml") },
+            };
+
+            CBTheme.ItemsSource = _themes;
+            CBTheme.SelectedValue = "Light";
+            CBTheme.DisplayMemberPath = "Value.ThemeTitle";
+            CBTheme.SelectedValuePath = "Key";
+        }
+        //Switch from theme according to the given paths.
+        private void ChangeTheme(Theme theme)
+        {
+            themeDictionary.Clear();
+            themeDictionary.MergedDictionaries.Add(new ResourceDictionary() { Source = theme.ThemeUri });
+            NavigationController.ChangeTheme();
+        }
+
+        private void CBTheme_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object value = CBTheme.SelectedValue;
+            Theme theme;
+            if (_themes.TryGetValue((string)value, out theme))
+            {
+                ChangeTheme(theme);
+            }
         }
 
         public override void OnLoad()
