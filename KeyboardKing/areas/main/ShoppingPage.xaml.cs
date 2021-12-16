656830add937f47d198975aaab0213fb71d8163a
CamielYang <Yangcamiel@gmail.com>
2021-12-16 19:07:04 +0100

    Added tooltip for names on hover, Added shelf img for items and polishes

--- KeyboardKing/areas/main/ShoppingPage.xaml.cs
+++ KeyboardKing/areas/main/ShoppingPage.xaml.cs
@@ -45,12 +45,14 @@ namespace KeyboardKing.areas.main
             {
                 List<List<string>> Items = new List<List<string>>
                 {
-                    new List<string> {"Naam1", "/KeyBoardking;component/resources/images/icons/coin.png", "10", "True"},
-                    new List<string> {"Naam2", "/KeyBoardking;component/resources/images/icons/coin.png", "100", "True"},
-                    new List<string> {"Naam3", "/KeyBoardking;component/resources/images/icons/coin.png", "250", "False"},
-                    new List<string> {"Naam4", "/KeyBoardking;component/resources/images/icons/coin.png", "500", "True"},
-                    new List<string> {"Naam5", "/KeyBoardking;component/resources/images/hellobeertje_background_4k.png", "1000", "False"},
-                    new List<string> {"Naam6", "/KeyBoardking;component/resources/images/icon.png", "10000", "False" }
+                    new List<string> {"Coin", "/KeyBoardking;component/resources/images/icons/coin.png", "10", "True"},
+                    new List<string> {"Coin", "/KeyBoardking;component/resources/images/icons/coin.png", "100", "True"},
+                    new List<string> {"Coin", "/KeyBoardking;component/resources/images/icons/coin.png", "250", "False"},
+                    new List<string> {"Coin", "/KeyBoardking;component/resources/images/icons/coin.png", "500", "True"},
+                    new List<string> {"Hello Beertje", "/KeyBoardking;component/resources/images/hellobeertje_background_4k.png", "1000", "False"},
+                    new List<string> {"Kroon", "/KeyBoardking;component/resources/images/icon.png", "10000", "False" },
+                    new List<string> {"Naam6", "/KeyBoardking;component/resources/images/icon.png", "10000", "False" },
+                    new List<string> {"Naam6", "/KeyBoardking;component/resources/images/icon.png", "10000", "False" }, 
                 };
                 LbShop.ItemsSource = Items;
             });
