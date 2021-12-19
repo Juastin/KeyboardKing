b2e0b15f429552c9f45a9b52c34c9cce1c289783
mareinvzx <35396160+mareinvzx@users.noreply.github.com>
2021-12-19 12:06:24 +0100

    kut component

---
+++ KeyboardKing/components/CoinsComp.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Controller;
+using Model;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace KeyboardKing.components
+{
+    /// <summary>
+    /// Interaction logic for Coin.xaml
+    /// </summary>
+    
+    public partial class CoinsComp : UserControl
+    {
+        public event EventHandler Load;
+        public Pages CurrentPage2 { get; set; } = Pages.Empty;
+
+        public CoinsComp()
+        {
+            InitializeComponent();
+            CoinsChange();
+        }
+
+        private void UserControl1_Load(Object sender, EventArgs e)
+        {
+
+            MessageBox.Show("You are in the UserControl.Load event.");
+        }
+
+        public void CoinsChange()
+        {
+            if(CurrentPage2 == Pages.ChaptersPage)
+            {
+                Coins.Content = EpisodeController.GetCoins((User)Session.Get("student"));
+            }else
+            {
+                Coins.Content = "2";
+            }
+        }
+    }
+}
