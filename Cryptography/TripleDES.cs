cb608ee527a006f672dd1cca87d5aecb9f0853cc
storm <stormtersteeglive@hotmail.com>
2021-12-20 12:08:53 +0100

    Added comments and turned CipherMode into a const

--- Cryptography/TripleDES.cs
+++ Cryptography/TripleDES.cs
@@ -4,8 +4,22 @@ using System.Text;
 
 namespace Cryptography
 {
+    /// <summary>
+    /// Encryption Algorithm class used to encrypt and decrypt string data.
+    /// </summary>
     public static class TripleDES
     {
+        /// <summary>
+        /// Specifies the block cipher mode to use for encryption.
+        /// </summary>
+        private const CipherMode Mode = CipherMode.ECB;
+
+        /// <summary>
+        /// Used to encrypt a data string 'source', using string 'key'.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static string Encrypt(string source, string key)
         {
             using (TripleDESCryptoServiceProvider tripleDESCryptoService = new TripleDESCryptoServiceProvider())
@@ -14,13 +28,19 @@ namespace Cryptography
                 {
                     byte[] byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
                     tripleDESCryptoService.Key = byteHash;
-                    tripleDESCryptoService.Mode = CipherMode.ECB;
+                    tripleDESCryptoService.Mode = Mode;
                     byte[] data = Encoding.UTF8.GetBytes(source);
                     return Convert.ToBase64String(tripleDESCryptoService.CreateEncryptor().TransformFinalBlock(data, 0, data.Length));
                 }
             }
         }
 
+        /// <summary>
+        /// Used to decrypt a data string 'encrypt', using string 'key'.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static string Decrypt(string encrypt, string key)
         {
             using (TripleDESCryptoServiceProvider tripleDESCryptoService = new TripleDESCryptoServiceProvider())
@@ -29,13 +49,19 @@ namespace Cryptography
                 {
                     byte[] byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
                     tripleDESCryptoService.Key = byteHash;
-                    tripleDESCryptoService.Mode = CipherMode.ECB;
+                    tripleDESCryptoService.Mode = Mode;
                     byte[] data = Convert.FromBase64String(encrypt);
                     return Encoding.UTF8.GetString(tripleDESCryptoService.CreateDecryptor().TransformFinalBlock(data, 0, data.Length));
                 }
             }
         }
 
+        /// <summary>
+        /// Static bool method used to quickly encrypt or decrypt data using a set key.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="encrypt"></param>
+        /// <returns></returns>
         public static string EncryptOrDecrypt(string source, bool encrypt)
         {
             return encrypt ? Encrypt(source, "332cc6da-d757-4e80-a726-0bf6b615df09") : Decrypt(source, "332cc6da-d757-4e80-a726-0bf6b615df09");
