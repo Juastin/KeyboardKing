ad05c3d29b0daf04562716444634aa4badf6c886
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2021-12-20 14:00:28 +0100

    Added summary

--- Cryptography/Argon2.cs
+++ Cryptography/Argon2.cs
@@ -8,8 +8,14 @@ namespace Cryptography
 {
     public static class Argon2
     {
-        // Argon2
-        //https://github.com/kmaragon/Konscious.Security.Cryptography
+        // Source:https://github.com/kmaragon/Konscious.Security.Cryptography
+
+        /// <summary>
+        /// Password will be hashed with the parameter salt and return in a encrypted string
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="salt"></param>
+        /// <returns></returns>
         public static string HashPassword(string password, string salt)
         {
             Argon2id argon2 = new(Encoding.UTF8.GetBytes(password));
@@ -20,6 +26,14 @@ namespace Cryptography
             return TripleDES.EncryptOrDecrypt(Convert.ToBase64String(argon2.GetBytes(16)), true);
         }
 
+        /// <summary>
+        /// Password will be compared with the other password. First and second parameter is for hashing password for verify password. 
+        /// It returns a bool if the passwords are equal.
+        /// </summary>
+        /// <param name="loginPw"></param>
+        /// <param name="salt"></param>
+        /// <param name="userPw"></param>
+        /// <returns></returns>
         public static bool VerifyHash(string loginPw, string salt, string userPw)
         {
             byte[] loginHashPw = Convert.FromBase64String(TripleDES.EncryptOrDecrypt(HashPassword(loginPw, salt), false));
@@ -27,6 +41,10 @@ namespace Cryptography
             return loginHashPw.SequenceEqual(userHashPw);
         }
 
+        /// <summary>
+        /// Salt generated with the RNGCryptoServiceProvider and return it encrypted
+        /// </summary>
+        /// <returns></returns>
         public static string CreateSalt()
         {
             byte[] salt = new byte[16];
