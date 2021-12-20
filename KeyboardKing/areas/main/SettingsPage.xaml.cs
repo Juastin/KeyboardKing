d98d00086b760a5ed6a24b941f61f250e08cd0d3
Justin Passchier <juspasschier@gmail.com>
2021-12-20 14:12:22 +0100

    Deleted click account delete button

--- KeyboardKing/areas/main/SettingsPage.xaml.cs
+++ KeyboardKing/areas/main/SettingsPage.xaml.cs
@@ -79,10 +79,5 @@ public SettingsPage(MainWindow w) : base(w)
         public override void OnTick()
         {
         }
-
-        private void DeleteAccountClick(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
