709a1068daf1eb1e3bc398104a6003dd0b3d2d47
Wouter <woutervandervelde2000@hotmail.com>
2021-12-21 23:38:07 +0100

    added example of how to use anonymous type (easier to explain this way tomorrow)

--- KeyboardKing/areas/play/MatchHistoryLeaderboardPage.xaml.cs
+++ KeyboardKing/areas/play/MatchHistoryLeaderboardPage.xaml.cs
@@ -3,6 +3,7 @@ using DatabaseController;
 using KeyboardKing.core;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace KeyboardKing.areas.play
 {
@@ -23,6 +24,25 @@ namespace KeyboardKing.areas.play
 
             List<List<string>> raw_result = DBQueries.GetAllScoresOrderByHighest(selected_matchid);
             // Parse the received data so the timestamps are readable
+
+            /**
+             * Easy way of creating an object without creating a whole new one in Model
+             * `actual_matches` is now a List of an anonymous type.
+             * You're able to call `actual_matches[0].EpisodeName` on it this way instead of `actualmatches[0][0]`
+             * 
+             * If you want to get rid of these changes just uncomment the old code and replace the bindings back in `MatchHistoryLeaderboardPage.xaml`
+             */
+            var actual_matches = raw_result.Select(m => new
+            {
+                EpisodeName = m[0],
+                Username = m[1],
+                Score = m[2],
+                Mistakes = m[3],
+                LPM = m[4],
+                TimeSpend = ((int)new TimeSpan(long.Parse(m[5])).TotalMinutes) + ":" + (((new TimeSpan(long.Parse(m[5])).TotalSeconds % 60) < 10) ? "0" : "") + ((int)new TimeSpan(long.Parse(m[5])).TotalSeconds % 60),
+            }).ToList();
+
+            /** Old code, remove when not needed. Still here so you can easily revert my changes
             List<List<string>> actual_matches = new List<List<string>>();
             foreach (List<string> part in raw_result)
             {
@@ -36,6 +56,9 @@ namespace KeyboardKing.areas.play
             }
 
             EpisodeNameLabel.Content = actual_matches[0][0];
+            */
+
+            EpisodeNameLabel.Content = actual_matches[0].EpisodeName;
             MatchHistoryList.ItemsSource = actual_matches;
             MatchHistoryList.Items.Refresh();
         }
