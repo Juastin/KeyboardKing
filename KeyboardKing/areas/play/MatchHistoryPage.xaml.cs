ac13e284b7fde9a40d05a48bdb94c82ff8d78712
storm <stormtersteeglive@hotmail.com>
2021-12-21 13:13:24 +0100

    first view created

---
+++ KeyboardKing/areas/play/MatchHistoryPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Controller;
+using DatabaseController;
+using KeyboardKing.core;
+using Model;
+using System.Windows.Data;
+
+namespace KeyboardKing.areas.play
+{
+    /// <summary>
+    /// Interaction logic for MatchHistoryPage.xaml
+    /// </summary>
+    public partial class MatchHistoryPage : JumpPage
+    {
+        public MatchHistoryPage(MainWindow w) : base(w)
+        {
+            InitializeComponent();
+        }
+
+        public override void OnLoad()
+        {
+            User user = (User)Session.Get("student");
+            ListCollectionView lcv = new ListCollectionView(DBQueries.GetAllEpisodes(user));
+            lcv.GroupDescriptions.Add(new PropertyGroupDescription("ChapterName"));
+            CBEpisode.ItemsSource = lcv;
+            CBEpisode.SelectedValuePath = "Id";
+        }
+
+        public override void OnShadow()
+        {
+        }
+
+        public override void OnTick()
+        {
+        }
+    }
+}
