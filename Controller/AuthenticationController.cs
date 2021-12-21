f847fcdb394cb0d29f6bad64876f39537c4b076d
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2021-12-21 13:13:35 +0100

    Update AuthenticationController.cs

--- Controller/AuthenticationController.cs
+++ Controller/AuthenticationController.cs
@@ -1,13 +1,8 @@
 ﻿using Cryptography;
 using DatabaseController;
 using Model;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Controller
 {
