2ad14e8619e6f565d43c8bc9ce107b362ec37a30
Justin Passchier <juspasschier@gmail.com>
2021-12-24 15:56:55 +0100

    Added summaries

--- Controller/SettingsController.cs
+++ Controller/SettingsController.cs
@@ -10,7 +10,7 @@ namespace Controller
         public static event EventHandler Refresh;
         private static Dictionary<string, Font> _fonts;
         private static ResourceDictionary fontDictionary = Application.Current.Resources.MergedDictionaries[1];
-        
+
         public static void Initialise()
         {
             _fonts = new Dictionary<string, Font>
@@ -19,17 +19,26 @@ namespace Controller
                 { "Verdana", new Font("Verdana", "resources/fonts/Verdana.xaml") },
             };
         }
-
+        /// <summary>
+        /// Refresh event that fires to refresh wpf checkboxes to show actual values.
+        /// </summary>
         public static void RefreshWpf()
         {
             Refresh?.Invoke(null, EventArgs.Empty);
         }
+        /// <summary>
+        /// Changes font based on the given Font.
+        /// </summary>
+        /// <param name="font"></param>
         private static void ChangeFont(Font font)
         {
             fontDictionary.Clear();
             fontDictionary.MergedDictionaries.Add(new ResourceDictionary() { Source = font.FontUri });
         }
-
+        /// <summary>
+        /// Changes font based on the dyslectic bool of the user. If true the font Verdana will be showed, otherwise SegeoUI will be showed.
+        /// </summary>
+        /// <param name="dyslectic"></param>
         public static void ChangeDyslecticFont(bool dyslectic)
         {
             if (dyslectic)
